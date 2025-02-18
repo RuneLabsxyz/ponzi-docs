49e27875a1ea8c3a99a2451d2ee22d1e56c0963c
Veikkan <pierre.semanne@gmail.com>
2025-02-18 14:37:24 +0100

    feat: add socials in the navbar

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,96 +1,112 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'Ponzi Land',
-  tagline: 'Buy, stake and farm different tokens at the same time avoiding high slippage | Or nuke it all',
-  favicon: 'img/coin.png',
+  title: "Ponzi Land",
+  tagline:
+    "Buy, stake and farm different tokens at the same time avoiding high slippage | Or nuke it all",
+  favicon: "img/coin.png",
 
   // Set the production url of your site here
-  url: 'https://ponzi.land',
+  url: "https://ponzi.land",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Runelabs', // Usually your GitHub org/user name.
-  projectName: 'ponzi.land', // Usually your repo name.
+  organizationName: "Runelabs", // Usually your GitHub org/user name.
+  projectName: "ponzi.land", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
+          sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/Runelabs/ponzi-docs',
+          editUrl: "https://github.com/Runelabs/ponzi-docs",
         },
         blog: {
           showReadingTime: true,
           feedOptions: {
-            type: ['rss', 'atom'],
+            type: ["rss", "atom"],
             xslt: true,
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/Runelabs/ponzi-docs',
+          editUrl: "https://github.com/Runelabs/ponzi-docs",
           // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: "img/docusaurus-social-card.jpg",
     navbar: {
-      title: 'Ponzi Land',
+      title: "Ponzi Land",
       logo: {
-        alt: 'Ponzi Land Logo',
-        src: 'img/coin.png',
+        alt: "Ponzi Land Logo",
+        src: "img/coin.png",
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Docs',
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Docs",
+        },
+        {
+          href: "https://x.com/ponzidotland",
+          position: "right",
+          className: "header-twitter-link",
+          "aria-label": "Twitter",
+        },
+        {
+          href: "https://discord.gg/ponziland",
+          position: "right",
+          className: "header-discord-link",
+          "aria-label": "Discord",
+        },
+        {
+          href: "https://github.com/RuneLabsxyz/PonziLand",
+          position: "right",
+          className: "header-github-link",
+          "aria-label": "GitHub repository",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Docs',
-              to: '/docs/intro',
+              label: "Docs",
+              to: "/docs/intro",
             },
           ],
         },
